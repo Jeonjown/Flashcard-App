f1c7f34fcba64c04d80a6f2652898043936e405c
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-07-12 14:17:13 +0800

    connect mongoDB uri to server

---
+++ server/model/userModel.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+//Deck
+    // title 
+    // FlashCards
+        // term
+        // definition
--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from "dotenv";
 dotenv.config();
-
 const server = express();
 
-const port = process.env.PORT;
+//connecting to database
+const dbConnect = async () => {
+    await mongoose.connect(process.env.DB_URI);
+    try {
+        console.log('connected to DB');
 
-server.listen(port, () => {
-    console.log(`listening to server ${port}`);
-});
+        // server
+        server.listen(process.env.PORT, () => {
+            console.log(`listening to server ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log('Error at dbConnect ::', error);
+    }
+};
+
+dbConnect();
+
