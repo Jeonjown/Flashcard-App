0ec3126eba9d9f9eade534ff99d1f406dbc3e9c7
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-07-14 20:44:58 +0800

    made signup controller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,11 +1,38 @@
+import { User } from "../model/userModel.js";
+import bcrypt from 'bcrypt';
+import validator from "validator";
+
 
 export const signupUser = async (req, res) => {
     const { email, password } = req.body;
-    res.json({ email, password });
+
+    //validate
+    if (!email || !password) {
+        res.status(401).json({ error: 'fill all fields' });
+    }
+    if (!validator.isEmail(email)) {
+        res.status(401).json({ error: 'not a valid email' });
+    }
+    if (!validator.isStrongPassword(password)) {
+        res.status(401).json({ error: 'password not strong enough' });
+    }
+
+    const existingUser = User.findOne({ email });
+
+    try {
+        if (existingUser) {
+            return res.status(401).json({ error: 'email already in used' });
+        }
+
+        res.json({ email });
+    } catch (error) {
+        res.status(401).json({ error });
+    }
+
 };
 
 export const loginUser = async (req, res) => {
-    res.status(200).json({ msg: 'login' });
+    s;
 };
 
 export const test = async (req, res) => {
