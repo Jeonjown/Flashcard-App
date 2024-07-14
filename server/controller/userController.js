44dd3aabf4084625ab8beead5a9d02a7fa87580e
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-07-14 21:32:34 +0800

    made user login

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,10 +1,17 @@
 import { User } from "../model/userModel.js";
-import bcrypt from 'bcrypt';
+import bcrypt, { genSalt } from 'bcrypt';
 import validator from "validator";
+import jwt from 'jsonwebtoken';
+
+function createToken(_id) {
+    const token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+
+    return token;
+}
 
 
 export const signupUser = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = await req.body;
 
     //validate
     if (!email || !password) {
@@ -17,24 +24,54 @@ export const signupUser = async (req, res) => {
         res.status(401).json({ error: 'password not strong enough' });
     }
 
-    const existingUser = User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
     try {
         if (existingUser) {
             return res.status(401).json({ error: 'email already in used' });
         }
 
-        res.json({ email });
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        const user = await User.create({ email, password: hash });
+
+        const token = createToken(user._id);
+        res.status(200).json({ user, token });
+
+        return user;
     } catch (error) {
-        res.status(401).json({ error });
+        res.status(401).json({ error: error.message });
     }
 
 };
 
 export const loginUser = async (req, res) => {
-    s;
-};
+    const { email, password } = await req.body;
 
-export const test = async (req, res) => {
-    res.status(200).json({ msg: 'test' });
+    const user = await User.findOne({ email });
+
+    try {
+        if (!user) {
+            return res.status(401).json({ error: 'no user exist. please sign up first' });
+        }
+
+        const matchPassword = await bcrypt.compare(password, user.password);
+
+        if (!matchPassword) {
+            return res.status(401).json({ error: 'password not matched' });
+        }
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ user, token });
+        return user;
+
+
+    } catch (error) {
+        return res.status(401).json({ error: error.message });
+    }
+
+
+
+
 };
