5aed9eee73197da8ece8960cfbf208c4feaa4765
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-08-05 17:25:48 +0800

    fix import bug

--- client/src/components/DecksRecent.jsx
+++ client/src/components/DecksRecent.jsx
@@ -1,4 +1,3 @@
-import React, from "react";
 import { Link } from "react-router-dom";
 import { useDeckContext } from "../hooks/useDeckContext";
 import { useAuthContext } from "../hooks/useAuthContext";
