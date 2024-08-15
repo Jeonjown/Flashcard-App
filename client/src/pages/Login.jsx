52b811c6552c0676760c7b756b5d73f185344b53
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-08-15 23:35:18 +0800

    fix loading

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,79 +15,82 @@ const Login = () => {
 
   return (
     <section className="relative">
-      {loading && (
-        <div>
+      {loading ? (
+        // Display the loading component when loading is true
+        <div className="flex h-screen items-center justify-center">
           <Loading />
         </div>
-      )}
-      <div className="mx-auto flex flex-col items-center px-6 py-8 md:h-screen lg:py-0">
-        <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-5 xl:p-0">
-          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
-              Sign in to your account
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-500"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-                  placeholder="name@email.com"
-                  required
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-500"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  autoComplete="current-password"
-                  placeholder="••••••••"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-                  required
-                  value={password}
-                />
-              </div>
+      ) : (
+        // Display the form when not loading
+        <div className="mx-auto flex flex-col items-center px-6 py-8 md:h-screen lg:py-0">
+          <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-5 xl:p-0">
+            <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-500"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
+                    placeholder="name@email.com"
+                    required
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="mb-2 block text-sm font-medium text-gray-500"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="current-password"
+                    placeholder="••••••••"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
+                    required
+                    value={password}
+                  />
+                </div>
 
-              <button
-                type="submit"
-                className="w-full rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                onClick={handleClick}
-              >
-                Sign in
-              </button>
-              {error && <p className="text-sm text-red-500">{error}</p>}
-              <p className="text-sm font-light text-gray-500">
-                Don’t have an account yet?{" "}
-                <Link to="/signup">
-                  <span className="font-medium hover:underline">Sign up</span>
-                </Link>
-              </p>
-            </form>
+                <button
+                  type="submit"
+                  className="w-full rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                  onClick={handleClick}
+                >
+                  Sign in
+                </button>
+                {error && <p className="text-sm text-red-500">{error}</p>}
+                <p className="text-sm font-light text-gray-500">
+                  Don’t have an account yet?{" "}
+                  <Link to="/signup">
+                    <span className="font-medium hover:underline">Sign up</span>
+                  </Link>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -14,90 +14,94 @@ const Signup = () => {
   };
 
   return (
-    <section>
-      <div className="mx-auto flex flex-col items-center px-6 py-8 md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="mb-6 flex items-center text-2xl font-semibold text-gray-900"
-        ></a>
-        <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-5 xl:p-0">
-          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
-              Sign up for an account
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="username"
-                  className="mb-2 block text-sm font-medium text-gray-500"
+    <section className="relative">
+      {loading ? (
+        // Display the loading component when loading is true
+        <div className="flex h-screen items-center justify-center">
+          <Loading />
+        </div>
+      ) : (
+        // Display the form when not loading
+        <div className="mx-auto flex flex-col items-center px-6 py-8 md:h-screen lg:py-0">
+          <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-5 xl:p-0">
+            <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
+                Sign up for an account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="mb-2 block text-sm font-medium text-gray-500"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
+                    placeholder="Your username"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-500"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
+                    placeholder="name@email.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="mb-2 block text-sm font-medium text-gray-500"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="••••••••"
+                    autoComplete="current-password"
+                    className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                  onClick={handleClick}
+                  disabled={loading} // Disable the button when loading
                 >
-                  Username
-                </label>
-                <input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  name="username"
-                  id="username"
-                  autoComplete="username"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-                  placeholder="Your username"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-500"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-                  placeholder="name@email.com"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-500"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="••••••••"
-                  autoComplete="current-password"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                onClick={handleClick}
-                disabled={loading} // Disable the button when loading
-              >
-                {loading ? <Loading /> : "Sign up"}{" "}
-                {/* Show loading indicator or text */}
-              </button>
-              {error && <p className="text-[12px] text-primary">{error}</p>}{" "}
-              {/* Show error message if any */}
-            </form>
+                  {loading ? <Loading /> : "Sign up"}{" "}
+                  {/* Show loading indicator or text */}
+                </button>
+                {error && <p className="text-[12px] text-primary">{error}</p>}{" "}
+                {/* Show error message if any */}
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
